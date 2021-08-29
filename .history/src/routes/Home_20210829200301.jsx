ad1f53f4077c3a884881e19593f2af2c7cdf1b5c
Lincoln Kantet <lincoln.kantet@gmail.com>
2021-08-29 20:04:16 +0300

    changes

--- .history/src/routes/Home_20210829200301.jsx
+++ .history/src/routes/Home_20210829200301.jsx
@@ -21,9 +21,8 @@ function Home() {
         Click to open a category Joke
       </p>
       <div className="container h-auto w-full overflow-hidden pl-12 lg:pl-24 pt-4 flex flex-wrap ">
-        {categories.map((cat, i) => (
-          <Card key={i} cat={cat} />
-        ))}
+        
+        {categories && categories.map((cat, i) => <Card key={i} cat={cat} />)}
       </div>
     </div>
   );
